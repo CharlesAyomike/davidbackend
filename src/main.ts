1494e5c5e77d37c9842e59facce9eb703174ba3c
CharlesAyomike <cayomike@gmail.com>
2025-02-05 23:34:08 +0100

    whitelist frontend url

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', "www.ayomidedavid.com.ng", "ayomidedavid.com.ng"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
